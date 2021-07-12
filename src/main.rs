864f7ff13e892249a9b7b4bd1145e4e0af33ce8d
Jonathan Hyland <85582235+jonathan-hyland@users.noreply.github.com>
2021-07-12 13:57:51 -0400

    First part complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+    
+    // println!("{:?}", args);
 }
